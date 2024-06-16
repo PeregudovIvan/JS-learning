b67b6e83b26520bc671080ecfc3d5762ea23282a
Ivan <yadnexmail@yandex.ru>
2024-06-17 00:16:20 +0300

    yet one boolean task

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
-"use strict";
+// const hamburger = 5;
+// const fries = 0;
 
-if (5) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
-}
-
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Too many');
-// } else {
-//     console.log('Ok!');
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
 // }
-// (num === 50) ? console.log('Ok!') : console.log('Error');
 
 
-const num = '50';
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
-switch (num) {
-    case '49':
-        console.log('false');
-        break;
-    case '100':
-        console.log('false');
-        break;
-    case '50':
-        console.log('true');
-        break;
-    default:
-        console.log('nothing true');
-        break;
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'sdfjois');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('Все довольны!');
+} else {
+    console.log('Мы уходим.');
 }
+
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
