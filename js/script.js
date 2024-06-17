9fec5005e6d8a90879cfb536d008e82f014aec0c
Ivan <yadnexmail@yandex.ru>
2024-06-17 21:40:02 +0300

    argument and function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,9 @@
-'use strict';
+const usdCur = 28;
+const eurCur = 32;
 
-
-let num = 20;
-
-
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 10;
-    console.log(num);
+function convert(amount, curr) {
+    console.log(curr * amount);
 }
 
-showFirstMessage("Hello World!");
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(4, 3))
-
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log('Hello');
-};
-
-logger();
-
-const calc = (a, b) => a + b;
+convert(500, usdCur);
+convert(500, eurCur);
