908b4fad7c7d10f3028826d5ca8f8fd287326528
Ivan <yadnexmail@yandex.ru>
2024-06-17 08:12:38 +0300

    cycles in js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,22 @@
-// const hamburger = 5;
-// const fries = 0;
+"use strict";
 
-// if (hamburger && fries) {
-//     console.log('Я сыт!');
-// }
+let num = 50;
 
-
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 1;
-
-// console.log(hamburger === 3 && cola && fries);
-
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'sdfjois');
-
-// if (hamburger === 3 && cola === 1 && fries) {
-//     console.log('Все сыты!');
-// } else {
-//     console.log('Мы уходим!');
-// }
-
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Все довольны!');
-} else {
-    console.log('Мы уходим.');
+while (num <= 55) {
+    console.log(num);
+    num++;
 }
 
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
 
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-console.log(!0);
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
