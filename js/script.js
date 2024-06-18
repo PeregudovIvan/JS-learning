4b6d30dfa7cd4db4452b6c3e279c84ed65afaee8
Ivan <yadnexmail@yandex.ru>
2024-06-18 16:04:45 +0300

    object counter and length object

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
 "use strict";
 
-function first() {
-    // Do something
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }, 
+    makeTest: function() {
+        console.log('Test');
+    }
+};
 
-    setTimeout( function() {
-        console.log(1);
-    }, 500);
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+console.log(options['colors']['border']);
+let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+        counter++;
+    }
 }
 
-function second() {
-    console.log(2);
+for (let key in options) {
+    // if (typeof(options[key]) === 'object') {
+    //     for (let i in options[key]) {
+    //         console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+    //     }
+    // } else {
+    //     console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+        counter++;
+    // }
 }
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
+console.log(counter);
